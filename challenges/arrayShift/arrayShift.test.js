159175c8df1fde203e3d0dc6f2aff8ff1d3a10d2
sohadQtaitat <qtaitat3@gmail.com>
2020-01-25 16:18:39 +0200

    re challenge because install problem :star2:

---
+++ challenges/arrayShift/arrayShift.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const shiftArray = require('./array-shift');
+
+describe('testing module', () => {
+  it('should be array as the first value', () => {
+    let expected = null;
+    let result = shiftArray.insertShiftArray(4, 4);
+    expect(result).toEqual(expected);
+  });
+
+
+
+  it('the defult empty array and 0', () => {
+    let expected = [0];
+    let result = shiftArray.insertShiftArray();
+    expect(result).toEqual(expected);
+  });
+
+  it('inserts second parameter in middle of the array', () => {
+    let expected = [5,10,15,20,25];
+    let result = shiftArray.insertShiftArray([5,10,20,25], 15);
+    expect(result).toEqual(expected);
+  })
+})
