06a32268c7beefc7691f83480cd167cb3b262abb
sohadQtaitat <qtaitat3@gmail.com>
2020-02-24 17:34:27 +0200

    code challenge 27 fix test

--- challenges/merge-sort/merge-sort.test.js
+++ challenges/merge-sort/merge-sort.test.js
@@ -2,9 +2,8 @@ const mergeSort = require('./merge-sort');
 
 describe('Merge sort', () => {
   it('sort the given array ', () => {
+    const arr = [3, 9, 4];
 
-    const arr = [5, 17, 4];
-
-    expect(mergeSort(arr)).toEqual([5, 17, 9]);
+    expect(mergeSort(arr)).toEqual([3, 4, 9]);
   });
 });
