4677628f0a0f62d80cb6e3b3dde28b5f385d89ec
sohadQtaitat <qtaitat3@gmail.com>
2020-02-24 05:03:49 +0200

    challenge 16 done whit eslint

--- challenges/insertionSort/insertionSort.js
+++ challenges/insertionSort/insertionSort.js
@@ -1,22 +1,22 @@
 function insertion(array) {
 
-    for (let i = 1; i < array.length; i++) {
+  for (let i = 1; i < array.length; i++) {
 
-        let j = i - 1;
+    let j = i - 1;
 
-        let temp = array[i];
+    let temp = array[i];
 
 
-        while (j >= 0 && temp < array[j]) {
-            array[j + 1] = array[j];
-            j = j - 1;
-        }
-        array[j + 1] = temp;
+    while (j >= 0 && temp < array[j]) {
+      array[j + 1] = array[j];
+      j = j - 1;
     }
+    array[j + 1] = temp;
+  }
 
 
 
-    return array;
+  return array;
 }
 //   console.log('result',insertion(array))
 module.exports = insertion;
