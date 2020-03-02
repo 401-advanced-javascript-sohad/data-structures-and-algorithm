5b0cf99de6f984c514ebb85f35f0cd1b1189cd34
sohadQtaitat <qtaitat3@gmail.com>
2020-03-02 14:29:17 +0200

    add test

--- challenges/tree-intersection/tree-intersections.test.js
+++ challenges/tree-intersection/tree-intersections.test.js
@@ -6,7 +6,13 @@ describe('challenge 32 tree-intersection sohad qtaitat test ', () => {
 
   let tree1 = [ 9, 7, 17, 22, 20, 4, 3, 6, 5 ];
   let tree2 = [ 41, 12, 91, 5, 3, 2, 72, 53, 87 ];
+  let tree3 = [ 35, 51, 14, 8, 63, 0,41, 18, 2 ];
+  let tree4 = [];
   let correctValues = [ 3, 5 ];
+
+
+
+
 
 
 
@@ -27,7 +33,29 @@ describe('challenge 32 tree-intersection sohad qtaitat test ', () => {
 
 
 
+  it('without correct values test ', () => {
+    let tree = new BST();
+    tree1.map(value => tree.add(value));
+    let ti1 = tree;
+    tree = new BST();
+    tree3.map(value => tree.add(value));
+    let ti3 = tree;
+    let newArr = tree.tree_intersection(ti1, ti3);
+    expect(newArr).toEqual([]);
+  });
 
 
+
+
+  it('empty tree test ', () => {
+    let tree = new BST();
+    tree1.map(value => tree.add(value));
+    let ti1 = tree;
+    tree = new BST();
+    tree4.map(value => tree.add(value));
+    let ti4 = tree;
+    let newArr = tree.tree_intersection(ti1, ti4);
+    expect(newArr).toEqual('Node Empty');
+  });
 
 });
